ce94210613bdb101df52fd50783794970fddfaca
Bemya <99530208+Bemya@users.noreply.github.com>
2022-10-18 22:31:51 +0300

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
+
 const request = require('request');
+const fs = require('fs');
+
 request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const results = JSON.parse(body).results;
-    console.log(results.reduce((count, movie) => {
-      return movie.characters.find((character) => character.endsWith('/18/'))
-        ? count + 1
-        : count;
-    }, 0));
+  if (error) {
+    console.error(error);
   }
-});
+  try {
+    fs.writeFile(process.argv[3], body, 'utf8', function (err, result) { if (err) console.log(err); });
+  } catch (err) {
+    console.log(err);
+  }
+})
